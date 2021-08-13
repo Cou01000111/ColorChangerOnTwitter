fc4297567f4c48ae1943a8e5ee651347e5bbfa4a
courange <courange.c@gmail.com>
2021-08-14 01:19:23 +0900

    fix bug with mouse hover

--- main.js
+++ main.js
@@ -13,12 +13,18 @@ function main(e) {
       if ((elLabel.indexOf('フォロー中') != -1)) {
         el.style.backgroundColor = 'rgb(29, 161, 242)';
         el.style.borderColor = 'rgba(0,0,0,0)';
+        el.addEventListener('mousemove', () => {
+          el.style.backgroundColor = 'rgb(220,30,41)';
+        });
+        el.addEventListener('mouseout', () => {
+          el.style.backgroundColor = 'rgb(29, 161, 242)';
+        });
       } else if ((elLabel.indexOf('フォロー') != -1)) {
         el.style.backgroundColor = 'rgba(0,0,0,0)';
         if (el.children[0].children[0].children[0])
           el.children[0].children[0].children[0].style.color = 'white';
         el.style.borderColor = 'rgb(83, 100, 113)';
-        el.addEventListener('mouseenter', () => {
+        el.addEventListener('mousemove', () => {
           el.style.backgroundColor = 'rgba(255,255,255,0.15)';
         });
         el.addEventListener('mouseout', () => {
